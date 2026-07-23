8495c34202eaa03c28755641f6d0637080d92eee
Al-Ahotanee <60036086+Al-Ahotanee@users.noreply.github.com>
2026-07-23 14:53:32 +0100

    Update SystemCore.php

--- SystemCore.php
+++ SystemCore.php
@@ -44,11 +44,12 @@ class SystemCore {
 
     private function connectDB(): void {
         $host = getenv('DB_HOST') ?: 'localhost';
+        $port = getenv('DB_PORT') ?: '1247';
         $db   = getenv('DB_NAME') ?: 'sazug_srms';
         $user = getenv('DB_USER') ?: 'root';
         $pass = getenv('DB_PASS') ?: '';
 
-        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
